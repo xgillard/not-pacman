c9dda4c484095b2f863435c59fead86a9d2a9b02
AGRAR\xavier.gillard <xaviergillard@gmail.com>
2024-03-13 20:17:36 +0100

    installer for windows

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ fn main() -> BResult<()> {
     let (w, h) = state.load_file("resources/map.txt").expect("could not load map");
     
     let context = BTermBuilder::new()
-        .with_title("not really pacman")
+        .with_title("pas cman")
         .with_dimensions(w, h)
         .with_fps_cap(30.0)
         .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
-        .with_font("pacman32.png", 32, 32)
+        .with_font("pas-cman-font-32.png", 32, 32)
         .with_font("terminal8x8.png", 8, 8)
-        .with_simple_console(w, h,"pacman32.png")
-        .with_simple_console_no_bg(w, h,"pacman32.png")
-        .with_simple_console_no_bg(w, h,"pacman32.png")
+        .with_simple_console(w, h,"pas-cman-font-32.png")
+        .with_simple_console_no_bg(w, h,"pas-cman-font-32.png")
+        .with_simple_console_no_bg(w, h,"pas-cman-font-32.png")
         .with_simple_console(w*2, h*2, "terminal8x8.png")
         .build()?;
 
